4de33b6ca656921161c1c0cb22dd7cb726fb4e5b
davi <davi.ghiggino@concilie.com.br>
2022-11-18 18:06:44 -0300

    feat: Implementando middlewares

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const path = require("path");
 const routes = require("./routes");
 const express = require("express");
 const app = express();
-
 const mongoose = require("mongoose");
+const {middlewareGlobal} = require("./src/middlewares/middleware");
+
 mongoose.connect(process.env.CONNECTIONSTRING)
     .then(() => app.emit("ready"))
     .catch(err => console.log(err));
 
+sapp.use(middlewareGlobal);
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
